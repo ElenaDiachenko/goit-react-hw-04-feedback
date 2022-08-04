e986bcf3d16010ab6d47444d49a736cdf4eba574
ElenaDiachenko <96205320+ElenaDiachenko@users.noreply.github.com>
2022-08-04 03:43:06 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,10 +5,24 @@ export class App extends Component{
   state = {
     good: 0,
     neutral: 0,
-    bad: 0
+    bad: 6
   };
+
+  countTotalFeedback = () => {
+    return Object.values(this.state).reduce((acc, value) => acc + value, 0)
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const totalFeedback = this.countTotalFeedback();
+    const goodFeedback = this.state.good;
+    
+    return `${goodFeedback > 0 ?  Math.ceil((goodFeedback /totalFeedback) * 100):goodFeedback}%`;
+  }
+  
   
   render() {
+    const test = this.countPositiveFeedbackPercentage()
+    console.log(test)
     return (
       <Container>
         <Title>Please leave feedback</Title>
@@ -22,6 +36,8 @@ export class App extends Component{
           {Object.entries(this.state).map(el => (
             <p key={el[0]}>{el[0].charAt(0).toUpperCase()+ el[0].slice(1)}: {el[1]}</p>
           ))}
+          <p>Total: {this.countTotalFeedback()}</p>
+          <p>Positive feedback: {this.countPositiveFeedbackPercentage()}</p>
         </Statistics>
 
       </Container>
