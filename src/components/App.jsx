2287fa6dff17756f2e9ce4e398d9dc62c69a89e0
ElenaDiachenko <96205320+ElenaDiachenko@users.noreply.github.com>
2022-08-04 03:04:23 +0300

    added start files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Container,Title,FeedbackOptions,Statistics } from './App.styled';
+
+export class App extends Component{
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  };
+  
+  render() {
+    return (
+      <Container>
+        <Title>Please leave feedback</Title>
+        <FeedbackOptions>
+          {Object.keys(this.state).map(key =>
+            <button key={key}>{key}</button>
+            )}
+        </FeedbackOptions>
+        <Title>Statistics</Title>
+        <Statistics>
+          {Object.entries(this.state).map(el => (
+            <p key={el[0]}>{el[0].charAt(0).toUpperCase()+ el[0].slice(1)}: {el[1]}</p>
+          ))}
+        </Statistics>
+
+      </Container>
+    )
+  }
+}
---
+++ src/components/App.styled.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin: 0 auto;
+`;
+
+
+export const Title = styled.h2`
+  font-size: 30px;
+  margin-bottom: 30px;
+`;
+export const FeedbackOptions = styled.div`
+ display: flex;
+ gap:20px;
+ `;
+
+export const Statistics = styled.div`
+ margin-bottom:10px;
+ font-size: 20px;
+ font-weight:500;
+`;
